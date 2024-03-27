d2883eec76e10d21e331393efd059ae9f0b13360
foglar <kohout.fi.2023@skola.ssps.cz>
2024-03-27 15:09:22 +0100

    arduino-cli helper script, bash_aliases

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -5,70 +5,53 @@ green=$(tput setaf 2)
 blue=$(tput setaf 4)
 reset=$(tput sgr0)
 
-pkg_installed()
-{
+pkg_installed() {
     local PkgIn=$1
 
-    if pacman -Qi $PkgIn &> /dev/null
-    then
-        #echo "${PkgIn} is already installed..."
+    if pacman -Qi "$PkgIn" &> /dev/null; then
         return 0
     else
-        #echo "${PkgIn} is not installed..."
         return 1
     fi
 }
 
-pkg_available()
-{
+pkg_available() {
     local PkgIn=$1
 
-    if pacman -Si $PkgIn &> /dev/null
-    then
-        #echo "${PkgIn} available in arch repo..."
+    if pacman -Si "$PkgIn" &> /dev/null; then
         return 0
     else
-        #echo "${PkgIn} not available in arch repo..."
         return 1
     fi
 }
 
-chk_aurh()
-{
-    if pkg_installed yay
-    then
+chk_aurh() {
+    if pkg_installed yay; then
         aurhlpr="yay"
-    elif pkg_installed paru
-    then
+    elif pkg_installed paru; then
         aurhlpr="paru"
     fi
 }
 
-aur_available()
-{
+aur_available() {
     local PkgIn=$1
     chk_aurh
 
-    if $aurhlpr -Si $PkgIn &> /dev/null
-    then
-        #echo "${PkgIn} available in aur repo..."
+    if "$aurhlpr" -Si "$PkgIn" &> /dev/null; then
         return 0
     else
-        #echo "aur helper is not installed..."
         return 1
     fi
 }
 
-if ! pkg_installed git
-    then
+if ! pkg_installed git; then
     echo "installing dependency git..."
     sudo pacman -S git
 fi
 
 chk_aurh
 
-if [ -z $aurhlpr ]
-    then
+if [ -z "$aurhlpr" ]; then
     echo -e "Select aur helper:\n1) yay\n2) paru"
     read -p "Enter option number : " aurinp
 
@@ -80,46 +63,57 @@ if [ -z $aurhlpr ]
     esac
 
     echo "installing dependency $aurhlpr..."
-    ./install_aur.sh $aurhlpr 2>&1
+    ./install_aur.sh "$aurhlpr" 2>&1
 fi
+
+install_category() {
+    category="$1"
+    echo "Installing packages from $category..."
+    while IFS= read -r pkg; do
+        if [ -n "$pkg" ]; then
+            if pkg_installed "$pkg"; then
+                echo "skipping $pkg..."
+            elif pkg_available "$pkg"; then
+                echo "queueing $pkg from arch repo..."
+                pkg_arch+=" $pkg"
+            elif aur_available "$pkg"; then
+                echo "queueing $pkg from aur..."
+                pkg_aur+=" $pkg"
+            else
+                echo "error: unknown package $pkg..."
+            fi
+        fi
+    done < <(cut -d '#' -f 1 "$category")
+
+    if [ -n "$pkg_arch" ]; then
+        echo "installing $pkg_arch from arch repo..."
+        sudo pacman "${use_default}" -S $pkg_arch
+    fi
+
+    if [ -n "$pkg_aur" ]; then
+        echo "installing $pkg_aur from aur..."
+        "$aurhlpr" "${use_default}" -S $pkg_aur
+    fi
+}
 
 install_list="${1:-$HOME/.local/share/packages/term-tools.lst}"
 
-while read pkg
-do
-    if [ -z $pkg ]
-        then
-        continue
+while read -r category; do
+    if [ -f "$category" ]; then
+        read -p "Do you want to install packages from $category? (y/n): " answer
+        if [ "$answer" = "y" ]; then
+            install_category "$category"
+        fi
     fi
+done <<<"games.lst
+hacking.lst
+hyprland.lst
+internet.lst
+nvidia.lst
+other.lst
+programming.lst
+term-tools.lst
+tools-apps.lst"
 
-    if pkg_installed ${pkg}
-        then
-        echo "skipping ${pkg}..."
-
-    elif pkg_available ${pkg}
-        then
-        echo "queueing ${pkg} from arch repo..."
-        pkg_arch=`echo $pkg_arch ${pkg}`
-
-    elif aur_available ${pkg}
-        then
-        echo "queueing ${pkg} from aur..."
-        pkg_aur=`echo $pkg_aur ${pkg}`
-
-    else
-        echo "error: unknown package ${pkg}..."
-    fi
-done < <( cut -d '#' -f 1 $install_list )
-
-if [ `echo $pkg_arch | wc -w` -gt 0 ]
-    then
-    echo "installing $pkg_arch from arch repo..."
-    sudo pacman ${use_default} -S $pkg_arch
-fi
-
-if [ `echo $pkg_aur | wc -w` -gt 0 ]
-    then
-    echo "installing $pkg_aur from aur..."
-    $aurhlpr ${use_default} -S $pkg_aur
-fi
+echo "Installation complete."
 
