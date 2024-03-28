c4a52c99870d660545ab12948680059cd66ed9c2
foglar <kohout.fi.2023@skola.ssps.cz>
2024-03-28 18:00:00 +0100

    neovim config added, setup_neofetch removed

---
+++ private_dot_config/nvim/lua/custom/configs/lspconfig.lua
@@ -0,0 +1,38 @@
+local config = require("plugins.configs.lspconfig")
+
+local on_attach = config.on_attach
+local capabilities = config.capabilities
+
+local lspconfig = require("lspconfig")
+local util = require "lspconfig/util"
+
+lspconfig.pyright.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"python"},
+})
+
+lspconfig.clangd.setup({
+  on_attach = function (client, bufnr)
+    client.server_capabilities.signatureHelpProvider = false
+    on_attach(client, bufnr)
+  end,
+  capabilities = capabilities,
+})
+
+lspconfig.gopls.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = {"gopls"},
+  filetypes = {"go", "gomod", "gowork", "gotmpl"},
+  root_dir = util.root_pattern("go.work", "go.mod", "git"),
+  settings = {
+    gopls = {
+      completeUnimported = true, -- This automatically imports packeges into code
+      usePlaceholders = true, -- This completes function parameters
+      analyses = {
+        unusedparams = true, -- Get unused parammeters warning
+      }
+    },
+  },
+})
