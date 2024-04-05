be3d55365e12f1a6521e6fc580d8d55fcb7cf08a
foglar <kohout.fi.2023@skola.ssps.cz>
2024-04-05 15:26:16 +0200

    fixed issues with evaluating installed packages

--- private_dot_config/dotfiles/scripts/executable_install_aur.sh
+++ private_dot_config/dotfiles/scripts/executable_install_aur.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source $HOME/.local/bin/setup_scripts/global_fn.sh
+source $HOME/.config/dotfiles/scripts/global_fn.sh
 if [ $? -ne 0 ] ; then
     echo "Error: unable to source global_fn.sh, please execute from $(dirname "$(realpath "$0")")..."
     exit 1
